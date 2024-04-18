5d2e8262841e723574000222f14fe2024e07eecb
cephas-n <cephas.developer@gmail.com>
2024-04-19 01:29:35 +0300

    validation create product

--- controllers/products/createProduct.js
+++ controllers/products/createProduct.js
@@ -1,10 +1,34 @@
 const apiError = require("../../exception/apiError");
 const Product = require("../../models/Product");
+const Store = require("../../models/Store");
+const yup = require("yup");
+
+const productSchema = yup
+  .object({
+    title: yup.string().max(255),
+    price: yup.number().min(0),
+    labels: yup.array(yup.string()).min(1),
+    images: yup.array(yup.string()).min(1),
+    storeId: yup.number().min(1),
+    description: yup.string().max(255),
+  })
+  .test("store should exist", async ({ storeId }, ctx) => {
+    const store = await Store().findByPk(storeId);
+    if (!store) {
+      return ctx.createError({ message: "Store Id does not exist" });
+    }
+    return true;
+  });
 
 const createProduct = async (req, res) => {
   try {
-    const { title, price, labels, description, images, storeId } =
-      await req.body;
+    const body = await req.body;
+
+    await productSchema.validate(body, {
+      abortEarly: true,
+    });
+
+    const { title, price, labels, description, images, storeId } = body;
 
     const { dataValues: product } = await Product().create({
       title,
--- exception/apiError.js
+++ exception/apiError.js
@@ -1,6 +1,18 @@
+const { ValidationErrorItem } = require("sequelize");
+const { ValidationError } = require("yup");
+
 const apiError = (err, res) => {
-  if (!res.headerSent) return;
-  res.status(500).send("Api Error");
+  if (res.headerSent) return;
+  console.error(err);
+  switch (true) {
+    case err instanceof ValidationError ||
+      err?.errors[0] instanceof ValidationErrorItem:
+      const { errors } = err;
+      res.status(422).send({ errors });
+      break;
+    default:
+      res.status(500).send("Api Error");
+  }
 };
 
 module.exports = apiError;
